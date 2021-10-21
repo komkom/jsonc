f885ea4032fb6d87655eaf6be4df75c82f2e99f4
michael <info@komika.org>
2021-10-21 09:49:18 +0200

    cmd depedency removed

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 
-	"github.com/alecthomas/kingpin"
 	"github.com/komkom/jsonc/jsonc"
-)
-
-var (
-	minimize = kingpin.Flag(`minimize`, `transform to minified json`).Short('m').Bool()
 )
 
 func main() {
 
-	f, err := jsonc.New(os.Stdin, *minimize, " ")
+	var minimize bool
+	flag.BoolVar(&minimize, "m", false, `transform to minified json`)
+	flag.Parse()
+
+	f, err := jsonc.New(os.Stdin, minimize, " ")
 	if err != nil {
 		fmt.Printf("no input stream, error: %v", err)
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/komkom/jsonc
 go 1.14
 
 require (
-	github.com/alecthomas/kingpin v2.2.6+incompatible
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
